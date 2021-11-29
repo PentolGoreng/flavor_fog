20334196efd9ab8a8574c7cb16d3b05cd679b3ae
PentolGoreng <frederickalexander_19@kharisma.ac.id>
2021-11-29 20:06:35 +0800

    list repairs

--- lib/screens/home/components/product_list.dart
+++ lib/screens/home/components/product_list.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flavor_fog/components/product_card1.dart';
+import 'package:flavor_fog/constants.dart';
 import 'package:flavor_fog/screens/details/details_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flavor_fog/components/product_card.dart';
+import 'package:flavor_fog/components/shop_card.dart';
 import 'package:flavor_fog/models/product.dart';
-
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:flavor_fog/components/listProduct.dart';
 import '../../../size_config.dart';
 import 'section_title.dart';
 
@@ -13,95 +15,117 @@ class ProductList extends StatefulWidget {
   State<ProductList> createState() => _ProductListState();
 }
 
-class _ProductListState extends State<ProductList> {
+class _ProductListState extends State<ProductList>
+    with AutomaticKeepAliveClientMixin {
+  bool get wantKeepAlive => true;
+  late int currentPage;
+  final PageController controller = PageController();
+  List<Widget> _buildScreens() {
+    return [
+     ListProduct(
+        menuScreenContext: widget.menuScreenContext,
+        hideStatus: _hideNavBar,
+        
+        
+      ),];}
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: SectionTitle(title: "Categories", press: () {}),
-        ),
-        SizedBox(height: getProportionateScreenWidth(20)),
-        Column(
-          children: [
-            // SizedBox(
-            //     height: getProportionateScreenHeight(
-            //         getProportionateScreenHeight(600)),
-            //     child:
-
-            StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection('products')
-                  // .orderBy('sentAt', descending: true)
-                  .snapshots(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                final productDB = snapshot.data.docs;
-
-                return GridView.count(
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  children: [
-                    ...List.generate(
-                      snapshot.data.docs.length,
-                      (index) {
-                        // Product(
-                        //     title: productDB[index]['title'],
-                        //     price: productDB[index]['price'],
-                        //     id: productDB[index]['id'],
-                        //     images: List.from(productDB[index]['images']),
-                        //     description: productDB[index]['desc']);
-                        // DetailsScreen(
-                        //     title: productDB[index]['title'],
-                        //     price: productDB[index]['price'],
-                        //     id: productDB[index]['id'],
-                        //     images: List.from(productDB[index]['images']),
-                        //     description: productDB[index]['desc']);
-                        return ProductCard1(
-                            title: productDB[index]['title'],
-                            price: productDB[index]['price'],
-                            id: productDB[index]['id'],
-                            images: List.from(productDB[index]['images']),
-                            description: productDB[index]['desc']);
-
-                        return SizedBox
-                            .shrink(); // here by default width and height is 0
-                      },
-                    ),
-                    // MessageBubble(
-                    //     message: chatDocuments[index]['text'],
-                    //     isMe: chatDocuments[index]['userId'] ==
-                    //         futureSnapshot.data.uid,
-                    //     key: ValueKey(chatDocuments[index].id),
-                    //     firstName: chatDocuments[index]['name'],
-                    //     imageUrl: chatDocuments[index]['image'],
-                    //   )
-                  ],
-                );
+    updateKeepAlive();
+    return Expanded(
+      child: Column(
+        children: [
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //   children: [
+          //     GestureDetector(
+          //         onTap: () async {
+          //           updateKeepAlive();
+          //           controller.animateToPage(0,
+          //               duration: defaultDuration, curve: Curves.linear);
+          //         },
+          //         child: Container(
+          //           decoration: BoxDecoration(),
+          //           height: 25,
+          //           color: login_bg,
+          //           width: MediaQuery.of(context).size.width * 0.5,
+          //           child: Center(child: Text("Products")),
+          //         )),
+          //     GestureDetector(
+          //       onTap: () async {
+          //         updateKeepAlive();
+          //         controller.animateToPage(1,
+          //             duration: defaultDuration, curve: Curves.linear);
+          //       },
+          //       child: Container(
+          //         height: 25,
+          //         color: login_bg,
+          //         width: MediaQuery.of(context).size.width * 0.5,
+          //         child: Center(child: Text("Shops")),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          
+          PersistentTabView(context, screens: ),
+          Expanded(
+            child: PageView(
+              controller: controller,
+              onPageChanged: (page) {
+                setState(() {
+                  currentPage = page;
+                });
               },
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      children: [
+                        // SizedBox(
+                        //     height: getProportionateScreenHeight(
+                        //         getProportionateScreenHeight(600)),
+                        //     child:
+
+                       
+                        // ),
+                        // GridView.count(crossAxisCount: 2, mainAxisSpacing: 30, children: [
+                        //   ...List.generate(
+                        //     products.length,
+                        //     (index) {
+                        //       return ProductCard1(product: products[index]);
+
+                        //       return SizedBox
+                        //           .shrink(); // here by default width and height is 0
+                        //     },
+                        //   ),
+                        // ]),
+
+                        // SizedBox(width: getProportionateScreenWidth(20)),
+                      ],
+                    ),
+                  ],
+                ),
+
+               
+
+                // ),
+                // GridView.count(crossAxisCount: 2, mainAxisSpacing: 30, children: [
+                //   ...List.generate(
+                //     products.length,
+                //     (index) {
+                //       return ProductCard1(product: products[index]);
+
+                //       return SizedBox
+                //           .shrink(); // here by default width and height is 0
+                //     },
+                //   ),
+                // ]),
+
+                // SizedBox(width: getProportionateScreenWidth(20)),
+              ],
             ),
-
-            // ),
-            // GridView.count(crossAxisCount: 2, mainAxisSpacing: 30, children: [
-            //   ...List.generate(
-            //     products.length,
-            //     (index) {
-            //       return ProductCard1(product: products[index]);
-
-            //       return SizedBox
-            //           .shrink(); // here by default width and height is 0
-            //     },
-            //   ),
-            // ]),
-
-            // SizedBox(width: getProportionateScreenWidth(20)),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
